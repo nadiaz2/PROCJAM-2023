e825ab1bb1795b24ca0cc5dc7b541df4ab56f418
Nelson Diaz <101888543+nadiaz2@users.noreply.github.com>
2023-12-07 01:44:34 -0500

    Wave function core (#2)
    * Incorporate heap into Grid class
    
    * Move class definition to sperate file
    
    * Began adding core functionality
    
    * Add entropy calculation to TileCell
    
    * Implement entropy & propogation code
    
    Currently there is still a bug with the tile enabler code & propogation.
    This causes the adjacency rules to not always be followed.
    
    * Fix bug in enablers causing undesireable results
    
    * Refactor output & seed printing

---
+++ src/waveCollapse/waveClasses.py
@@ -0,0 +1,165 @@
+from dataclasses import dataclass
+from heapq import heappush, heappop
+import random
+from waveExceptions import *
+from math import log2
+from enum import Enum
+from copy import deepcopy
+import time
+
+
+class Direction(Enum):
+	UP = 0
+	RIGHT = 1
+	DOWN = 2
+	LEFT = 3
+
+
+@dataclass
+class RemovalUpdate:
+	"""Represents that the stored tileIndex was removed from the possibilities
+	of the cell at the stored coordinate."""
+	tileIndex: int
+	coord: tuple[int,int]
+
+
+@dataclass(frozen = True, order = True)
+class EntropyCoord:
+	"""Immutable class that stores the entropy for a specific coord"""
+	entropy: float
+	coord: tuple[int, int]
+
+
+class EntropyHeap:
+	"""Maintains the heap of entropy values for cells"""
+	_heap: list[EntropyCoord]
+
+	def __init__(self):
+		self._heap = []
+
+	def push(self, item: EntropyCoord):
+		heappush(self._heap, item)
+
+	def pop(self) -> EntropyCoord:
+		return heappop(self._heap)
+
+	def __iter__(self):
+		return self
+	
+	def __next__(self):
+		try:
+			return self.pop()
+		except(IndexError):
+			raise StopIteration
+
+
+class TileCell:
+	"""Class for keeping track of the state of individual cells in the grid."""
+	collapsed: bool
+	chosenTile: int
+	possible: list[bool]  # indexed by tile_index
+	_sum_of_possible_weights: int
+	_sum_of_possilbe_weight_log_weight: float
+	_entropy_noise: float
+	tile_enabler_counts: list[list[int]]  # outer list is indexed by tile_index, inner list is indexed by Direction
+
+	def __init__(self, frequencyHints: list[int], enablerList: list[list[int]]):
+		self.collapsed = False
+		self.chosenTile = -1
+		self.possible = [True]*len(frequencyHints)
+		self._sum_of_possible_weights = sum(frequencyHints)
+		self._sum_of_possilbe_weight_log_weight = sum([freq*log2(freq) for freq in frequencyHints])
+		self._entropy_noise = random.random() / 100_000
+		self.tile_enabler_counts = deepcopy(enablerList)
+
+	def collapse(self, frequencyHints: list[int]) -> bool:
+		weightedList = []
+		for i, possible in enumerate(self.possible):
+			if(not possible):
+				continue
+			
+			for _ in range(frequencyHints[i]):
+				weightedList.append(i)
+		
+		if(len(weightedList) == 0):
+			return False
+
+		self.chosenTile = weightedList[random.randint(0, len(weightedList)-1)]
+		self.collapsed = True
+		self.possible = [i == self.chosenTile for i in range(len(self.possible))]
+		
+		return True
+
+	def entropy(self) -> float:
+		"""Returns the current entropy of this cell."""
+		# The \ allows the code to carry onto the next line. It has no effect on the computation.
+		return log2(self._sum_of_possible_weights) \
+			- (self._sum_of_possilbe_weight_log_weight / self._sum_of_possible_weights) \
+			+ self._entropy_noise
+
+	def removeTile(self, tile_index: int, freq_hints: list[int]):
+		"""Removes a tile from the 'possible' list and updates the cached entropy values accordingly."""
+		self.possible[tile_index] = False
+
+		freq = freq_hints[tile_index]
+		self._sum_of_possible_weights -= freq
+		self._sum_of_possilbe_weight_log_weight -= freq * log2(freq)
+
+
+class Grid:
+	"""The entire grid of cells. Contains redundant information to make access easier (ex. size)"""
+	cells: list[list[TileCell]]
+	size: tuple[int,int]
+	heap: EntropyHeap
+	seed: int
+
+	def __init__(self, width: int, height: int, adjacencyRules: list[tuple(list[int])], frequencyHints: list[int], given_seed: int = None):
+		self.size = (width, height)
+		self.cells = [[None for i in range(height)] for j in range(width)]
+		self.heap = EntropyHeap()
+		
+		if(given_seed == None):
+			given_seed = int(time.time())
+		random.seed(given_seed)
+		self.seed = given_seed
+
+		defaultEnablers = Grid._getDefaultEnablers(adjacencyRules)
+
+		for x in range(width):
+			for y in range(height):
+				cell = TileCell(frequencyHints, defaultEnablers)
+				self.cells[x][y] = cell
+				self.heap.push(EntropyCoord(cell.entropy(), (x,y)))
+
+	def getFinalTileList(self) -> list[list[int]]:
+		width, height = self.size
+		
+		tiles = [[None for _ in range(height)] for _ in range(width)]
+
+		for x in range(width):
+			for y in range(height):
+				if(self.cells[x][y].collapsed):
+					tiles[x][y] = self.cells[x][y].chosenTile
+				else:
+					raise NotReadyException("Not all cells have been collapsed.")
+		
+		return tiles
+	
+	@staticmethod
+	def _getDefaultEnablers(adjacencyRules: list[tuple[list[int]]]) -> list[list[int]]:
+		tile_count = len(adjacencyRules)
+		defaultEnablers = [[0,0,0,0] for _ in range(tile_count)]
+
+		# This for loop calculates the number of tiles that enable each tile from a specific direction.
+		# For example, defaultEnablers[tile_a][Direction.RIGHT.value] would be the number of possibilities
+		# from the RIGHT side that allow tile_a to exist in a given cell.
+		#
+		# This calculation relies on the adjacencyRules being consistent. This means that if tile_a
+		# enables tile_b to be placed on its left, then tile_b must also enable tile_a to be placed
+		# on its right. Therefore the number of tiles that tile_a enables in a given direction is equal
+		# to the number of tiles that enable it from the opposite direction.
+		for index in range(tile_count):
+			for dir in Direction:
+				defaultEnablers[index][dir.value] = len(adjacencyRules[index][dir.value])
+		
+		return defaultEnablers
--- src/waveCollapse/waveFunction.py
+++ src/waveCollapse/waveFunction.py
@@ -1,75 +1,106 @@
-from dataclasses import dataclass
-import heapq
-import random
+# The following link taught us a majority of what we have learned about the wave function collapse algorithm.
+# We drew heavy insperation from this article and used the author's Rust code as a basis for our implementation.
+# https://www.gridbugs.org/wave-function-collapse/
 
-@dataclass
-class TileCell:
-	"""Class for keeping track of the state of individual cells in the grid."""
-	collapsed: bool
-	possible: list
-	entropy_noise: float
-
-	def __init__(self, tileSetSize):
-		self.collapsed = False
-		self.possible = [True]*tileSetSize
-		self.entropy_noise = random.random() / 1000
-
-@dataclass
-class Grid:
-	"""The entire grid of cells. Contains redundant information to make access easier (ex. size)"""
-	cells: list[list[TileCell]]
-	size: tuple[int,int]
-
-	def __init__(self, width, height, tileSetSize):
-		self.size = (width, height)
-		self.cells = [[TileCell(tileSetSize)]*height]*width
-
-@dataclass(frozen = True, order = True)
-class EntropyCoord:
-	"""Immutable class that stores the entropy for a specific coord"""
-	entropy: float
-	coord: tuple[int, int]
+from waveClasses import *
 
 
-tileGrid: Grid = None
-entropyHeap = list()
+def wfc_core(adjacencyRules: list[tuple[list[int]]], frequencyRules: list[int], outputSize: tuple[int,int], seed: int = None, printOutput: bool = False):
+	tileGrid = Grid(outputSize[0], outputSize[1], adjacencyRules, frequencyRules, seed)
+	removalStack: list[RemovalUpdate] = []
 
-def Main():
-	#global entropyHeap
-	#heapq.heapify(entropyHeap) #sorts the current list into a heap structure
-	#heapq.heappush(entropyHeap, EntropyCoord(10.0, (0,0))) #inserts the given element into entropyHeap, assuming heap structure
-	#heapq.heappop(entropyHeap) #pops the smallest element off the heap and maintains heap structure
+	for item in tileGrid.heap:
+		x, y = item.coord
+		cell = tileGrid.cells[x][y]
+		if(cell.collapsed):
+			continue
+		
+		# Collapse chosen cell
+		oldPossible = cell.possible
+		success = cell.collapse(frequencyRules)
+		if(not success):
+			# TODO: restart
+			raise NotImplementedError("Yet to implement redo or abort on contradiction.")
 
-	#for x in range(size[0]):
-	#	for y in range(size[1]):
-	#		heapq.heappush(entropyHeap, EntropyCoord(random.random()*100, (x,y)))
+		# Begin propogation
+		oldPossible[cell.chosenTile] = False  # the chosen tile is still a valid enabler
 
-	#heapq.heappush(entropyHeap, EntropyCoord(10.0, (0,0)))
-	#print(heapq.heappop(entropyHeap))
-	global tileGrid
-	tileGrid = Grid(200, 200, 5)
-	print("done")
-	#for i in range(len(entropyHeap)):
-	#	print(heapq.heappop(entropyHeap))
+		# The stack should be empty at the moment. This just adds all needed RemovalUpdates
+		removalStack = [RemovalUpdate(index, item.coord) for index,pastPossible in enumerate(oldPossible) if pastPossible]
 
-def getNeighborSet(coord: tuple[int,int]):
+		_updatePossible(tileGrid, removalStack, adjacencyRules, frequencyRules)
+
+	output = tileGrid.getFinalTileList()
+
+	if(printOutput):
+		print(f'SEED: {tileGrid.seed}')
+		_printTileList(output)
+	
+	return output
+
+
+def _getNeighborSet(coord: tuple[int,int], gridSize: tuple[int,int]) -> list[tuple[Direction, tuple[int,int]]]:
+	"""Returns a list of coordinate pairs that are directly adjacent to the given coord.
+	Coordinates that lie outside the given gridSize are not returned in the list."""
 	def inBounds(variable):
-		x, y = variable
+		x, y = variable[1]
 
-		global tileGrid
-		if(x >= tileGrid.size[0] or x < 0):
+		if(x >= gridSize[0] or x < 0):
 			return False
-		if(y >= tileGrid.size[1] or y < 0):
+		if(y >= gridSize[1] or y < 0):
 			return False
 		
 		return True
 
-	offsets = [(-1,0), (1,0), (0,-1), (0,1)]
-	neighbors = [(coord[0]+x, coord[1]+y) for x,y in offsets]
+	offsets = [(Direction.LEFT, (-1,0)), (Direction.RIGHT, (1,0)), (Direction.UP, (0,-1)), (Direction.DOWN, (0,1))]
+	neighbors = [(dir, (coord[0]+x, coord[1]+y)) for dir,(x,y) in offsets]
 	neighbors = list(filter(inBounds, neighbors))
 
 	return neighbors
 
+
+def _updatePossible(tileGrid: Grid, removalStack: list[RemovalUpdate], adjacencyRules: list[tuple[list[int]]], frequencyHints: list[int]):
+	while(len(removalStack) > 0):
+		removal = removalStack.pop()
+		tileRemoved = removal.tileIndex
+		coord = removal.coord
+		for dir,neighborCoord in _getNeighborSet(coord, tileGrid.size):
+			x,y = neighborCoord
+			neighborCell = tileGrid.cells[x][y]
+			oppositeDir = (dir.value+2) % 4
+
+			if(neighborCell.collapsed):
+				continue
+
+			# for all tiles that tileRemoved enables via adjacencyRules
+			for enabledTile in adjacencyRules[tileRemoved][dir.value]:
+				if(not neighborCell.possible[enabledTile]):
+					continue
+
+				neighborCell.tile_enabler_counts[enabledTile][oppositeDir] -= 1
+
+				if(neighborCell.tile_enabler_counts[enabledTile][oppositeDir] == 0):
+					neighborCell.removeTile(enabledTile, frequencyHints)
+					tileGrid.heap.push(EntropyCoord(neighborCell.entropy(), neighborCoord))
+					removalStack.append(RemovalUpdate(enabledTile, neighborCoord))
+
+
+def _printTileList(tileList):
+	output = ""
+	for y in range(len(tileList[0])):
+		for x in range(len(tileList)):
+			output += str(tileList[x][y]) + " "
+		output += '\n'
+	print(output)
+
 if __name__ == "__main__":
-	Main()
-	getNeighborSet((0,1))
+	wfc_core(
+		[
+			([0], [0,1], [0,1], [0,1]),
+			([0,1], [0,1], [1], [0,1])
+		],
+		[1, 2],
+		(20, 10),
+		seed=None,
+		printOutput=True
+	)
